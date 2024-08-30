818ec285b4ab8cdb3e9e42e5ce49a43fa382f281
Oswald-Osei-Owusu-Asiedu <oswaldosei9@gmail.com>
2024-08-30 17:06:11 +0000

    added caching and transaction management

--- src/main/java/springdata/springdata/services/BedService.java
+++ src/main/java/springdata/springdata/services/BedService.java
@@ -1,34 +1,58 @@
 package springdata.springdata.services;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 import springdata.springdata.dtos.BedDto;
 import springdata.springdata.entities.Bed;
 import springdata.springdata.repositories.BedRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class BedService {
+    private final TransactionTemplate transactionTemplate;
     BedRepository bedRepository;
-    public BedService(BedRepository bedRepository) {
+    public BedService(BedRepository bedRepository, TransactionTemplate transactionTemplate) {
         this.bedRepository = bedRepository;
+        this.transactionTemplate = transactionTemplate;
     }
 
+    @Transactional
+    @CachePut(value = "bedCache", key = "#result.id")
     public BedDto addBed(BedDto bedDto) {
-     Bed newBed = convertToEntity(bedDto);
-     bedRepository.save(newBed);
+     Bed newBed = bedRepository.save(convertToEntity(bedDto));
      return convertToDto(newBed);
     }
 
+
+
     public BedDto addBedCustomQuery(BedDto bedDto) {
-        bedRepository.addBed(bedDto.getBedNumber(), bedDto.getWard().getId());
+        transactionTemplate.execute(status-> {
+            bedRepository.addBed(bedDto.getBedNumber(), bedDto.getWard().getId());
+            return bedDto;
+        });
         return bedDto;
     }
 
+
+    @Cacheable(value = "bedCache", unless = "#result==null")
+    public List<BedDto> getAllBeds() {
+        List<Bed> beds = bedRepository.findAll();
+        return beds.stream().map(this::convertToDto).toList();
+    }
+
+    @Cacheable(value = "bedCache", unless = "#result==null")
     public BedDto getBedById(Long id) {
         return convertToDto(bedRepository.findById(id).orElseThrow());
     }
 
+    @Transactional
+    @CachePut(value = "bedCache", key = "#bedDto.id")
     public BedDto updateBed(BedDto bedDto) {
         BedDto editedBedDto = new BedDto();
         if (bedDto.getId()!=null){
@@ -45,6 +69,7 @@ public class BedService {
         return editedBedDto;
     }
 
+    @CacheEvict(value = "bedCache", key = "#id")
     public void deleteBed(long id) {
         bedRepository.deleteById(id);
     }
